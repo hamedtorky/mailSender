47d3815cea66eeb86c3fb65e308e7220468a605b
hamedorky <hamedtorky2@gmail.com>
2015-09-19 13:44:27 +0430

    first commit

---
+++ main.py
@@ -0,0 +1,97 @@
+# __author__ = 'hamedtorky'
+import time
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+
+mailconfig = open("mailconfig.txt",'r')
+
+listName = mailconfig.readline().strip()
+
+from_address = mailconfig.readline().strip()
+username = from_address
+password = mailconfig.readline().strip()
+
+to_address = mailconfig.readline().strip()
+ccMail = to_address
+bccMail = to_address
+
+smtpAddress = mailconfig.readline().strip()
+mailconfig.close()
+print 'send mail by "'+from_address+'"'
+print 'static mail is "'+to_address+'"'
+print ''
+
+sleepTime = 2#second
+
+counter = 0
+
+Subject = 'test message!'
+text = 'in ik barname azmaishi mibashad . mitavanid in email ra pak konid'
+fileHtml = open("html.txt",'r')
+html = fileHtml.read()
+fileHtml.close()
+
+# -----------------------------------------------------------------
+mailBuffer = []
+mailList = open(listName,"r")
+
+endSendMail = 0
+print 'Start send mail in list "' + listName+'"'
+print '---------------------------------------------------'
+while endSendMail == 0:
+
+    for i in xrange(20):
+        mailBuffer.append(mailList.readline().strip())
+        ccMail = ccMail + ',' + mailBuffer[i]
+        bccMail = bccMail + ',' + mailBuffer[i]
+
+    for i in xrange(20):
+
+        print mailBuffer[i]
+        if mailBuffer[i] == "end":
+            endSendMail = 1
+            print "wait close program "
+            break
+        counter = counter + 1
+        to_address = mailBuffer[i]
+
+        msg = MIMEMultipart()
+
+        msg['From'] = from_address
+        msg['To'] = to_address
+        msg['Subject'] = Subject
+        msg['Bcc'] = bccMail
+        msg['Cc'] = ccMail
+
+        msg.attach(MIMEText(text,'plain'))
+        msg.attach(MIMEText(html,'html'))
+        msg.attach(MIMEImage(file("img/image1.png").read()))
+        msg.attach(MIMEImage(file("img/image2.png").read()))
+        msg.attach(MIMEImage(file("img/image3.png").read()))
+        msg.attach(MIMEImage(file("img/image4.png").read()))
+        msg.attach(MIMEImage(file("img/image5.png").read()))
+        msg.attach(MIMEImage(file("img/image6.png").read()))
+        msg.attach(MIMEImage(file("img/image7.png").read()))
+
+
+        server = smtplib.SMTP(smtpAddress)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(username, password)
+        server.sendmail(from_address, to_address, msg.as_string())
+        server.quit()
+
+        print 'send email for ',to_address
+        print 'Done.'
+        print '---------------------------------------------------'
+
+
+    ccMail = to_address
+    bccMail = to_address
+    time.sleep(sleepTime)
+mailList.close()
+print "Program Ended :p"
+
